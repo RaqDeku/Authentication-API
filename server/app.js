8a943887e8fbbace254e2d2e4fa60f545182c37b
RaqDeku <raqdeku@outlook.com>
2023-06-19 16:47:57 +0100

    Refactoring

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const createServer = () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: "http://localhost:5173/",
     })
   );
 
